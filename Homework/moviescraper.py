82f470841cd8f7637b26aaeb8e868770ce2dd0df
teskav <teskavaessen@hotmail.com>
2019-04-08 12:09:43 +0200

    Finished moviescraper.py

---
+++ Homework/moviescraper.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+# Name: Teska Vaessen
+# Student number: 11046341
+"""
+This script scrapes IMDB and outputs a CSV file with highest rated movies.
+"""
+
+import csv
+from requests import get
+from requests.exceptions import RequestException
+from contextlib import closing
+from bs4 import BeautifulSoup
+
+TARGET_URL = "https://www.imdb.com/search/title?title_type=feature&release_date=2008-01-01,2018-01-01&num_votes=5000,&sort=user_rating,desc"
+BACKUP_HTML = 'movies.html'
+OUTPUT_CSV = 'movies.csv'
+
+
+def extract_movies(dom):
+    """
+    Extract a list of highest rated movies from DOM (of IMDB page).
+    Each movie entry should contain the following fields:
+    - Title
+    - Rating
+    - Year of release (only a number!)
+    - Actors/actresses (comma separated if more than one)
+    - Runtime (only a number!)
+    """
+
+    # extract the list of highest rated movies
+    movies_IMDB = dom.find_all("div", class_="lister-item-content")
+    movies = []
+    one_movie = []
+
+    for movie in movies_IMDB:
+
+        # append the title
+        one_movie.append(movie.h3.a.string)
+
+        # append the rating
+        one_movie.append(movie.div.div.strong.string)
+
+        # append the year of release
+        spans = movie.h3.find_all("span")
+        one_movie.append(spans[1].string.strip("()I "))
+
+        # append the actors
+        films = movie.find_all("a")
+        stars = ""
+        for film in films:
+            if "_st_" in film['href']:
+                if stars == "":
+                    stars += film.string
+                else:
+                    stars += ", " + film.string
+        one_movie.append(stars)
+
+        # append the runtime
+        one_movie.append(movie.p.find("span", class_="runtime").string.strip("nim "))
+
+        # append movie to the list of movies
+        movies.append(one_movie)
+        one_movie = []
+
+    # ADD YOUR CODE HERE TO EXTRACT THE ABOVE INFORMATION ABOUT THE
+    # HIGHEST RATED MOVIES
+    # NOTE: FOR THIS EXERCISE YOU ARE ALLOWED (BUT NOT REQUIRED) TO IGNORE
+    # UNICODE CHARACTERS AND SIMPLY LEAVE THEM OUT OF THE OUTPUT.
+
+    return [movies]   # REPLACE THIS LINE AS WELL IF APPROPRIATE
+
+
+def save_csv(outfile, movies):
+    """
+    Output a CSV file containing highest rated movies.
+    """
+    writer = csv.writer(outfile)
+    writer.writerow(['Title', 'Rating', 'Year', 'Actors', 'Runtime'])
+
+    for movie in movies:
+        writer.writerows(movie)
+    # ADD SOME CODE OF YOURSELF HERE TO WRITE THE MOVIES TO DISK
+
+
+def simple_get(url):
+    """
+    Attempts to get the content at `url` by making an HTTP GET request.
+    If the content-type of response is some kind of HTML/XML, return the
+    text content, otherwise return None
+    """
+    try:
+        with closing(get(url, stream=True)) as resp:
+            if is_good_response(resp):
+                return resp.content
+            else:
+                return None
+    except RequestException as e:
+        print('The following error occurred during HTTP GET request to {0} : {1}'.format(url, str(e)))
+        return None
+
+
+def is_good_response(resp):
+    """
+    Returns true if the response seems to be HTML, false otherwise
+    """
+    content_type = resp.headers['Content-Type'].lower()
+    return (resp.status_code == 200
+            and content_type is not None
+            and content_type.find('html') > -1)
+
+
+if __name__ == "__main__":
+
+    # get HTML content at target URL
+    html = simple_get(TARGET_URL)
+
+    # save a copy to disk in the current directory, this serves as an backup
+    # of the original HTML, will be used in grading.
+    with open(BACKUP_HTML, 'wb') as f:
+        f.write(html)
+
+    # parse the HTML file into a DOM representation
+    dom = BeautifulSoup(html, 'html.parser')
+
+    # extract the movies (using the function you implemented)
+    movies = extract_movies(dom)
+
+    # write the CSV file to disk (including a header)
+    with open(OUTPUT_CSV, 'w', newline='') as output_file:
+        save_csv(output_file, movies)
